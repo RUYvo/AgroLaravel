edcb71183e2b21f1c3e026457f1e1fe0492cec96
RUYvo <engs-ruyjunior@camporeal.edu.br>
2024-05-16 22:19:20 -0300

    laravel muito bala, php muito ruim

---
+++ routes/web.php
@@ -0,0 +1,12 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProdutoController;
+
+Route::resource('products', ProdutoController::class);
+Route::post('products', [ProdutoController::class, 'store'])->name('products.store');
+Route::get('products/create', [ProdutoController::class, 'create'])->name('products.create');
+Route::get('products/{id}/edit', [ProdutoController::class, 'edit'])->where('id', '[0-9]+');
+Route::put('products/{id}', [ProdutoController::class, 'update'])->where('id', '[0-9]+');
+Route::patch('products/{id}', [ProdutoController::class, 'update'])->where('id', '[0-9]+');
+Route::delete('products/{id}', [ProdutoController::class, 'destroy'])->where('id', '[0-9]+');
